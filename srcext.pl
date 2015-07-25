6aeee400a4710590105210e95c99c0a2e11b28da
Yutaka Sugawara <sugawarayg@gmail.com>
2015-07-25 12:47:05 +0900

    This commits the first test of collect_recur subroutine.

--- srcext.pl
+++ srcext.pl
@@ -13,6 +13,11 @@ sub getconf {
 	my @sl = @ARGV;
 	my %r = ( 'dest' => $opt_d, 'absdest' => $opt_a, 'srclist' => \@sl );
 	return \%r;
-};
+}
+
+sub collect_recur {
+	my ($a, $b, $c, $d) = @_;
+	$c->{$a} = 1;
+}
 
 1;
--- srcext_test.pl
+++ srcext_test.pl
@@ -1,11 +1,5 @@
 use strict;
 
-#my @b = (1, 2, 3, 4, 5);
-#@b = @b[1..$#b];
-#foreach (@b) {
-#	print $_ . ", ";
-#}
-
 # This script tests the script below.
 require "./srcext.pl";
 
@@ -21,36 +15,56 @@ sub setat {
 	return $a;
 };
 
-# arg indice
-my $DEST = 1;
-my $ABSDEST = 3;
-my $SRC1ST = 4;
+sub getconf_test {
+	# arg indice
+	my $DEST = 1;
+	my $ABSDEST = 3;
+	my $SRC1ST = 4;
 
-my $c;	# config reference
-my @sl;	# source list
+	my $c;	# config reference
+	my @sl;	# source list
 
-$c = &getconf(&genarg());
+	$c = &getconf(&genarg());
 
-@sl = @{$c->{'srclist'}};
-defined $sl[0] or die 5;
+	@sl = @{$c->{'srclist'}};
+	defined $sl[0] or die '05';
 
-$c = &getconf(&setat(&genarg(), $DEST, './dest2'));
-$c->{'dest'} eq './dest2' or die 6;
-$c = &getconf(&setat(&genarg(), $DEST, './dest1'));
-$c->{'dest'} eq './dest1' or die 7;
+	$c = &getconf(&setat(&genarg(), $DEST, './dest2'));
+	$c->{'dest'} eq './dest2' or die '06';
+	$c = &getconf(&setat(&genarg(), $DEST, './dest1'));
+	$c->{'dest'} eq './dest1' or die '07';
 
-$c = &getconf(&setat(&genarg(), $ABSDEST, './destabs1'));
-$c->{'absdest'} eq './destabs1' or die 8;
-$c = &getconf(&setat(&genarg(), $ABSDEST, './destabs2'));
-$c->{'absdest'} eq './destabs2' or die 9;
+	$c = &getconf(&setat(&genarg(), $ABSDEST, './destabs1'));
+	$c->{'absdest'} eq './destabs1' or die '08';
+	$c = &getconf(&setat(&genarg(), $ABSDEST, './destabs2'));
+	$c->{'absdest'} eq './destabs2' or die '09';
 
-@sl = @{&getconf(&setat(&genarg(), $SRC1ST, 'file2'))->{'srclist'}};
-$sl[0] eq 'file2' or die 10;
-@sl = @{&getconf(&setat(&genarg(), $SRC1ST, 'file1'))->{'srclist'}};
-$sl[0] eq 'file1' or die 11;
+	@sl = @{&getconf(&setat(&genarg(), $SRC1ST, 'file2'))->{'srclist'}};
+	$sl[0] eq 'file2' or die '0A';
+	@sl = @{&getconf(&setat(&genarg(), $SRC1ST, 'file1'))->{'srclist'}};
+	$sl[0] eq 'file1' or die '0B';
 
-my @a;	# tmp array
-@a = @{&genarg()};
-push @a, ('file2');
-@sl = @{&getconf(\@a)->{'srclist'}};
-scalar @sl == 2 or die 12;
+	my @a;	# tmp array
+	@a = @{&genarg()};
+	push @a, ('file2');
+	@sl = @{&getconf(\@a)->{'srclist'}};
+	scalar @sl == 2 or die '0C';
+}
+&getconf_test();
+
+use Errno;
+
+my $sa;
+my %ha;
+my %hb;
+my $TESTDIR = 'testdir';
+my @INCDIRS;
+$! = 0;
+if (!mkdir $TESTDIR) {
+	$! == $!{EEXIST} or die 'test 1';
+}
+open($sa, '>', "$TESTDIR/a") or die 'test 2';
+close($sa);
+&collect_recur('"a"', $TESTDIR, \%ha, \%hb);
+keys %ha == 1 or die '10';
+keys %hb == 0 or die '11';
